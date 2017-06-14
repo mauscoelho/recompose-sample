bfb73a8ce578b23a67c7b2e8bd32ca71fdc38c93
Mauricio Coelho <mauscoelho@gmail.com>
2017-06-14 17:52:06 -0300

    :smile: add locales

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { IntlProvider } from "react-intl";
 import App from "./pages/App/App";
 import registerServiceWorker from "./registerServiceWorker";
 
-const styles = {
-  app: {
-    display: "flex",
-    flex: 1,
-    margin: 0,
-    padding: 0
-  }
-};
-
 ReactDOM.render(
-  <App style={styles.app} />,
+  <IntlProvider locale="en">
+    <App />
+  </IntlProvider>,
   document.getElementById("root")
 );
 registerServiceWorker();
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { compose, withState, withHandlers } from "recompose";
+import { compose, withState, withHandlers, defaultProps } from "recompose";
+import { FormattedMessage } from "react-intl";
 import logo from "./logo.svg";
 
 const styles = {
@@ -23,37 +24,41 @@ const styles = {
   },
   appLogo: {
     animation: "App-logo-spin infinite 20s linear",
-    height: 80,
-    transform: "rotate(0deg)",
-    WebkitTransform: "rotate(360deg)"
-  },
-  appIntro: {
-    fontSize: "large"
-  }
+    height: 80,    
+  },  
 };
 
-const increment = ({ setCounter, count }) => e => setCounter(++count);
-
-const enhance = compose(
-  withState("count", "setCounter", 0),
-  withHandlers({
-    increment
-  })
-);
-
-function App({ count, increment }) {
+function App({ count, increment, name }) {
   return (
     <div style={styles.app}>
       <div style={styles.appHeader}>
         <img src={logo} style={styles.appLogo} className="" alt="logo" />
-        <h2>Welcome to React Counter {count}</h2>
+        <h2>
+          <FormattedMessage
+            id="welcome"
+            defaultMessage={`Welcome {name}, you have {count, number} {count, plural,
+                      one {message}
+                      other {messages}
+                    }`}
+            values={{ name: <b>{name}</b>, count }}
+          />
+        </h2>
         <button style={styles.button} onClick={increment}>Increment</button>
-      </div>
-      <p style={styles.appIntro}>
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+      </div>      
     </div>
   );
 }
+
+const increment = ({ setCounter, count }) => e => setCounter(++count);
+
+const enhance = compose(
+  defaultProps({
+    name: "Mauricio"    
+  }),
+  withState("count", "setCounter", 1),
+  withHandlers({
+    increment
+  })
+);
 
 export default enhance(App);
